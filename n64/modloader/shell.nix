832e494ed72de1a7fbf6b8ef8c1e5d46d2f96faa
Dustin Lacewell <dlacewell@gmail.com>
2022-01-09 23:52:09 -0600

    Add typescript to the modloader shell

--- n64/modloader/shell.nix
+++ n64/modloader/shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = pkgs.callPackage ./deps.nix {};
+  buildInputs = (pkgs.callPackage ./deps.nix {}) ++ [ pkgs.nodePackages.typescript ];
 #   shellHook = ''
 #     cp ${./sfml_audio.node} ${out}/ModLoader64/emulator/sfml_audio.node;
 #   '';
